ad7dac9cb59d1120bac583e203b9ac73adc86c4d
Patrick Murray <contact@patmurraydev.com>
2018-09-11 20:52:30 +1000

    first commit

---
+++ js/index.js
@@ -0,0 +1,93 @@
+
+function Paginator(items, page, per_page) {
+
+  var page = page || 1,
+    per_page = per_page || 7,
+    offset = (page - 1) * per_page,
+
+    paginatedItems = items.slice(offset).slice(0, per_page),
+    total_pages = Math.ceil(items.length / per_page);
+  return {
+    page: page,
+    per_page: per_page,
+    pre_page: page - 1 ? page - 1 : null,
+    next_page: (total_pages > page) ? page + 1 : null,
+    total: items.length,
+    total_pages: total_pages,
+    data: paginatedItems
+  };
+}
+
+
+function layOutScreen(feed) {
+
+  $("#grid").empty();
+
+  var apps = feed;
+
+  var count = (apps.length / 7) + 1;
+
+  var width = window.innerWidth
+
+  console.log(width)
+  var size = window.innerHeight / 7
+
+  console.log(size)
+
+  var repeat = width / ((count-1) * (size + 2))  
+
+  console.log(repeat)
+
+  console.log(apps.length);
+
+  if ( repeat > 1 ) {
+    for (let index = 0; index < repeat; index++)  {
+      apps = apps.concat(apps)
+    }
+  }
+
+  console.log(apps.length);
+
+  count = (apps.length / 7) + 1;
+
+
+  for (let index = 1; index < count; index++) {
+    var elements = Paginator(apps, index)
+    if (elements.data.length == 7) {
+      var subgrid = $('<div class="sub-grid"></div>');
+      elements.data.forEach(app => {
+        var url = app.artworkUrl100;
+        var obj = '<div style="background-image: URL(' + url + ')"></div>'
+        $(subgrid).append(obj)
+      });
+      $("#grid").append(subgrid)
+    }
+  }
+
+  $('#grid').marquee({
+    //speed in milliseconds of the marquee
+    duration: 10000,
+    //gap in pixels between the tickers
+    gap: 0,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true,
+    allowCss3Support: false
+   });
+}
+
+$( "body" ).dblclick(function() {
+  var elem = document.body;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  }
+});
---
+++ js/itunes.js
@@ -0,0 +1,45 @@
+function getTopApps() {
+    var settings = {
+        "async": true,
+        "url": "apps.json",
+        "method": "GET",
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        var apps = response.feed.results;
+
+        layOutScreen(apps);
+    });
+}
+
+function getAppWithId(appID) {
+    var url = "https://itunes.apple.com/lookup?id=" + appID
+    var settings = {
+        "async": true,
+        "url": url,
+        "method": "GET",
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        var apps = response.feed.results;
+
+        saveApps([apps[0]]);
+    });
+}
+
+function saveApps(array) {
+    var old = localStorage.getItem("apps");
+    if(old === null) old = [];
+    var newData = old.concat(array);
+    localStorage.setItem("apps", newData);
+}
+
+function getApps() {
+    var localapps = localStorage.getItem("apps");
+    if (localapps.length > 0) {
+        layOutScreen(apps);
+    }
+    return localStorage.getItem("apps");
+}
